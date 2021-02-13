0aadf7b6990d58bed2903d1531c159d6855986e4
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-02-12 21:35:49 -0800

    updated process

--- process1.sh
+++ process1.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
-VFILE1=top_mod.v
-VFILE2=top_mod_m.v
+VFILE1=a_top_mod_old.v
+VFILE2=a_top_mod_new.v
 DUT=top_mod
 
-./firrtl-1.4.0 -i top_mod.fir
+echo "firrtl"
+./firrtl-1.5-SNAPSHOT \
+  --dont-fold div \
+  -i a_top_mod.fir \
+  -o $VFILE1
 
-verilator --lint-only $VFILE1
+echo "firrtl lint"
+verilator \
+  --top-module top_mod \
+  --lint-only \
+  $VFILE1
 
+echo "firtool"
 ../../llvm/circt/build/bin/firtool \
-  top_mod.fir \
+  a_top_mod.fir \
   --lower-to-rtl \
+  --enable-lower-types \
   --verilog -o=$VFILE2
 
-verilator --lint-only $VFILE2
+echo "firtool lint"
+verilator \
+  --top-module top_mod \
+  --lint-only \
+  $VFILE2
 
+echo "yosys 0"
 ../../YosysHQ/yosys/yosys -q -p "
-  read_verilog $VFILE1
+  read_verilog -sv $VFILE1
   rename $DUT top1
   proc
   memory
   flatten top1
   hierarchy -top top1
-  read_verilog $VFILE2
+  read_verilog -sv $VFILE2
   rename $DUT top2
   proc
   memory
@@ -33,4 +48,82 @@ verilator --lint-only $VFILE2
   equiv_simple -short
   equiv_induct
   equiv_status -assert
+"
+
+echo "yosys 1"
+../../YosysHQ/yosys/yosys -q -p "
+  read_verilog -sv $VFILE1
+  rename $DUT top1
+  proc
+  memory
+  flatten top1
+  hierarchy -top top1
+  read_verilog -sv $VFILE2
+  rename $DUT top2
+  proc
+  memory
+  flatten top2
+  equiv_make top1 top2 equiv
+  hierarchy -top equiv
+  clean -purge
+  equiv_simple -short -undef
+  equiv_induct
+  equiv_status -assert
+"
+
+echo "yosys 2"
+../../YosysHQ/yosys/yosys -q -p "
+  read_verilog -sv $VFILE1
+  rename $DUT top1
+  proc
+  memory
+  flatten top1
+  hierarchy -top top1
+  read_verilog -sv $VFILE2
+  rename $DUT top2
+  proc
+  memory
+  flatten top2
+  equiv_make top1 top2 equiv
+  hierarchy -top equiv
+  clean -purge
+  equiv_simple -short
+  equiv_induct -undef
+  equiv_status -assert
+"
+
+echo "yosys 3"
+../../YosysHQ/yosys/yosys -q -p "
+  read_verilog -sv $VFILE1
+  rename $DUT top1
+  proc
+  memory
+  flatten top1
+  hierarchy -top top1
+  read_verilog -sv $VFILE2
+  rename $DUT top2
+  proc
+  memory
+  flatten top2
+  clean -purge
+  miter -equiv -make_assert -flatten top1 top2 miter
+  sat -verify -prove-asserts -enable_undef -show-public miter
+"
+
+echo "yosys 4"
+../../YosysHQ/yosys/yosys -q -p "
+  read_verilog -sv $VFILE1
+  rename $DUT top1
+  proc
+  memory
+  flatten top1
+  hierarchy -top top1
+  read_verilog -sv $VFILE2
+  rename $DUT top2
+  proc
+  memory
+  flatten top2
+  clean -purge
+  miter -equiv -make_assert -flatten top1 top2 miter
+  sat -verify -prove-asserts -enable_undef -max_undef -show-public miter
 "
