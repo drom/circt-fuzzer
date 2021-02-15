0aaa33e51f4cf4c9e873657742596dd1b848f2cc
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-02-15 15:03:48 -0800

    cleaunp

--- infinite-loop.sh
+++ infinite-loop.sh
@@ -4,35 +4,58 @@ VFILE1=top_mod.v
 VFILE2=top_mod_m.v
 DUT=top_mod
 
+echo "
+<script src=\"main.js\"></script>
+<style>
+table { width: 100%; border-collapse: collapse; }
+table, th, td { border: 1px solid black; }
+th, td { padding: 6px; }
+td { white-space: pre; }
+</style>
+<table id=\"log\">
+<row>
+<th>seed</th>
+<th>firrtl</th>
+<th>lint</th>
+<th>mlir</th>
+<th>lint</th>
+<th>yosys</th>
+</row>"
+
 for (( s=1; s<=10000; s++ ))
 do
-  echo $s
+  echo "<tr>"
+
+  echo "<td>$s</td>"
   ./bin/cli.js \
     --seed $s \
-    --max-width 10 \
-    --max-inputs 10 \
-    --max-ops 10 \
+    --max-width 20 \
+    --max-inputs 20 \
+    --max-ops 50 \
     fir > top_mod.fir
     # -L false \
 
-  echo "1-old"
+  echo "<td>"
   ./firrtl-1.4.0 -i top_mod.fir
+  echo "</td>"
 
-  echo "2-old-verilator"
+  echo "<td>"
   verilator --lint-only $VFILE1
+  echo "</td>"
 
-  echo "3-new"
+  echo "<td>"
   ../../llvm/circt/build/bin/firtool \
     top_mod.fir \
     --lower-to-rtl \
     --verilog -o=$VFILE2
+  echo "</td>"
 
-  echo "4-new-verilator"
+  echo "<td>"
   verilator --lint-only $VFILE2
-    # -Wno-WIDTH \
+  echo "</td>"
 
 
-  echo "5-yosys"
+  echo "<td>"
   ../../YosysHQ/yosys/yosys -q -p "
     read_verilog $VFILE1
     rename $DUT top1
@@ -52,4 +75,6 @@ do
     equiv_induct
     equiv_status -assert
   "
+  echo "</td>"
+  echo "</tr>"
 done
